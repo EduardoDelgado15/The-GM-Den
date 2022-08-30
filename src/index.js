e645e60a95e4070a5a667c24f4f242fb05b46687
EduardoDelgado15 <Eddydelgado15@yahoo.com>
2022-08-29 21:36:44 -0700

    running local host

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_URI, DB_NAME } = process.env;
+
+//process.env.DB_URI
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -18,18 +24,16 @@ const typeDefs = gql`
   }
 `;
 
-
 const books = [
     {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
+        title: "The Awe",
+        author: "Kate Chopin",
     },
     {
-        title: 'City of Glass',
-        author: 'Paul Auster',
+        title: "City of Glass",
+        author: "Paul Auster",
     },
 ];
-
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
@@ -40,29 +44,31 @@ const resolvers = {
 };
 
 const {
-    ApolloServerPluginLandingPageLocalDefault
-} = require('apollo-server-core');
+    ApolloServerPluginLandingPageLocalDefault,
+} = require("apollo-server-core");
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    csrfPrevention: true,
-    cache: 'bounded',
-    /**
-     * What's up with this embed: true option?
-     * These are our recommended settings for using AS;
-     * they aren't the defaults in AS3 for backwards-compatibility reasons but
-     * will be the defaults in AS4. For production environments, use
-     * ApolloServerPluginLandingPageProductionDefault instead.
-    **/
-    plugins: [
-        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
-});
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const start = async () => {
+    const client = new MongoClient(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    await client.connect();
+    constdb = client.db(DB_NAME);
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        csrfPrevention: true,
+        cache: "bounded",
+        plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    });
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
+};
+
+
+start();
