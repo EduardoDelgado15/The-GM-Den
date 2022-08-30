cf83005a433aa89d22930a1733bec0054593f284
EduardoDelgado15 <Eddydelgado15@yahoo.com>
2022-08-29 19:56:50 -0700

    Lets get this started

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ const { ApolloServer, gql } = require('apollo-server');
 // your data.
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
-
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
@@ -23,31 +21,31 @@ const typeDefs = gql`
 
 const books = [
     {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+        title: 'The Awakening',
+        author: 'Kate Chopin',
     },
     {
-      title: 'City of Glass',
-      author: 'Paul Auster',
+        title: 'City of Glass',
+        author: 'Paul Auster',
     },
-  ];
+];
 
 
-  // Resolvers define the technique for fetching the types defined in the
+// Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+        books: () => books,
     },
-  };
+};
 
-  const {
+const {
     ApolloServerPluginLandingPageLocalDefault
-  } = require('apollo-server-core');
-  
-  // The ApolloServer constructor requires two parameters: your schema
-  // definition and your set of resolvers.
-  const server = new ApolloServer({
+} = require('apollo-server-core');
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     csrfPrevention: true,
@@ -60,13 +58,11 @@ const resolvers = {
      * ApolloServerPluginLandingPageProductionDefault instead.
     **/
     plugins: [
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
-  });
-  
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+});
 
-  
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
